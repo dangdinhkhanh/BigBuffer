59c119a57bb0b69ddfa0dcc4b36730638d550b6c
dinhkhanh <khanhdd85@gmail.com>
2015-03-05 17:29:17 +0100

    fix Index_Mapping.m

--- Index_Mapping.m
+++ Index_Mapping.m
@@ -2,13 +2,27 @@ function [ index_array ] = Index_Mapping( M,B )
 %INDEX_MAPPING Summary of this function goes here
 %   Detailed explanation goes here
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% mapping state (d,c1,c2,..,CB) to an index of transional states
+% mapping state (d,c[B],c[B-1],..,c[1]) to an index of transional states
 % M: number of messages need to be delivered to a destination
 % B: buffer size 
+% base to make values distint and increasing
+base = 100;
 % index is assigned for state
 i = 1;
+index_array = [];
 % the number of drops
-
+for d = 0:(M-B)
+    list = Generate(B,M-d);
+    % remove 2 first elements of lists (sum = 0,1)
+    list = list(2:end,:); 
+    for j = 1:size(list,1)
+        index_array(i) = d* base^B;
+        for k = 1:B
+            index_array(i) = index_array(i) + list(j,k)*base^(B-k);
+        end
+        i = i + 1;
+    end
+end
 
 
 end
